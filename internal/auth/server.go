218d5c1b54e4a1093c7ed139123427aa39e42564
Che Kwas <che.kwas@outlook.com>
2022-05-30 11:27:33 +0800

    feat: new server

---
+++ internal/auth/server.go
@@ -0,0 +1,58 @@
+package apiserver
+
+import (
+	"github.com/che-kwas/iam-kit/logger"
+	"github.com/che-kwas/iam-kit/server"
+)
+
+type authServer struct {
+	*server.Server
+	name string
+	log  *logger.Logger
+
+	err error
+}
+
+// NewServer builds a new authServer.
+func NewServer(name string) *authServer {
+	s := &authServer{
+		name: name,
+		log:  logger.L(),
+	}
+
+	return s.initStore().newServer().registerRouter()
+}
+
+// Run runs the authServer.
+func (s *authServer) Run() {
+	s.log.Sync()
+
+	if s.err != nil {
+		s.log.Fatal("failed to build the server: ", s.err)
+	}
+
+	if err := s.Server.Run(); err != nil {
+		s.log.Fatal("server stopped unexpectedly: ", err)
+	}
+}
+
+func (s *authServer) initStore() *authServer {
+	return s
+}
+
+func (s *authServer) newServer() *authServer {
+	if s.err != nil {
+		return s
+	}
+
+	s.Server, s.err = server.NewServer(s.name)
+	return s
+}
+
+func (s *authServer) registerRouter() *authServer {
+	if s.err != nil {
+		return s
+	}
+
+	return s
+}
